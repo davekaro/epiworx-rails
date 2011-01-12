e3d8c8b6ef9d4596619313ea44f9ea08a7e12113
Dave Kroondyk <dkroondyk@epitecgroup.com>
2011-01-11 22:36:36 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EpiworxRails::Application.config.secret_token = '33cca71bfe812e5794bc92014e828b44fa2a5aa8a3106cc12b9f4cd9ec91237af579081ee0ad87bc1e9fa52d9b92c88cf32d8247b30825c362072876bb5b0c4b'
