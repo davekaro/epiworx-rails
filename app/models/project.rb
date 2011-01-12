43346249a286bb875b0bf3b82cb003c2d6655f61
Dave Kroondyk <davekaro@gmail.com>
2011-01-11 23:56:29 -0500

    add final validations to project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,8 @@
 class Project < ActiveRecord::Base
-  validates :name, :presence => true,
-            :length => { :maximum => 100 },
-            :uniqueness => true
+  validates :name, 
+            :presence => true,
+            :uniqueness => true,
+            :length => { :maximum => 100 }
+  validates_length_of :notes, :maximum => 300
+  validates_length_of :description, :maximum => 300
 end
